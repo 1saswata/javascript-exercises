51c3603556e3843583020a65e93f0b700a7e4e11
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2024-05-09 11:46:15 +0530

    feat: Solve the exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldestPerson;
+    function getAge(birthYear, deathYear) {
+        deathYear = !deathYear? new Date().getFullYear() : deathYear;
+        return deathYear - birthYear;
+    }
+    return array.reduce((oldestPerson, currentPerson)=>
+                getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath) >
+                getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath) ?
+                oldestPerson : currentPerson);
 };
 
 // Do not edit below this line
